cc59db7374d201fc0180c2b3d4edb289a65fcf89
Boss <oncelemon02@gmail.com>
2022-04-11 16:36:25 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,41 @@
 import { useEffect } from "react";
+// import liff from "@line/liff";
 import "../styles/globals.css";
 
 const liffId = process.env.NEXT_PUBLIC_LIFF_ID;
 
 function MyApp({ Component, pageProps }) {
-  useEffect(async () => {
+  // useEffect(async () => {
+  //   // const liff = (await import("@line/liff")).default;
+  //   const liff = await import("@line/liff").default;
+  //   try {
+  //     await liff.init({ liffId });
+  //     console.log("liffliff;", liff);
+  //   } catch (error) {
+  //     console.error("liff init error", error.message);
+  //   }
+  //   if (!liff.isLoggedIn()) {
+  //     liff.login();
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    initLiff();
+  });
+
+  const initLiff = async () => {
     const liff = (await import("@line/liff")).default;
+    // const liff = await import("@line/liff").default;
     try {
       await liff.init({ liffId });
+      console.log("liffliff;", liff);
     } catch (error) {
       console.error("liff init error", error.message);
     }
     if (!liff.isLoggedIn()) {
       liff.login();
     }
-  });
+  };
 
   return <Component {...pageProps} />;
 }
